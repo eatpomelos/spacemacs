279ed99f8c8902d0adb4200c3383abcce2ea09cd
pomelo <2845748931@qq.com>
2019-07-31 15:40:36 +0800

    update

--- layers/pomelo-org/config.el
+++ layers/pomelo-org/config.el
@@ -1,2 +1,18 @@
 ;; 设置.txt为后缀的文件用org-mode打开
 (add-to-list 'auto-mode-alist '("\\.txt\\'" . org-mode))
+
+;; (org-babel-do-load-languages
+;;  'org-babel-load-languages
+;;  '((emacs-lisp . t)
+;;    (C . t)
+;;    (perl . t)
+;;    (java . t)
+;;    (js . t)
+;;    (ruby . t)
+;;    (ditaa . t)
+;;    (python . t)
+;;    ;;下面一行语句在不用的时候可以注释掉，但是不注释的话会出现一个警告
+;;    ;;(shell . t)
+;;    (latex . t)
+;;    (plantuml . t)
+;;    (R . t)))
