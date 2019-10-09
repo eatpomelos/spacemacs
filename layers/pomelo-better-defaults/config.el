60ba8f7cadbddb0844bb35d79490dbd790244b9b
pomelo <2845748931@qq.com>
2019-10-09 20:27:47 +0800

    update

--- layers/pomelo-better-defaults/config.el
+++ layers/pomelo-better-defaults/config.el
@@ -10,7 +10,7 @@
 ;;; License: GPLv3
 
 (global-linum-mode t)
-;; recentf不保存一下后缀的文件,这里要注意的是在emacs中正则表达式的'\'要写成'\\'
+;; recentf不保存以下后缀的文件,这里要注意的是在emacs中正则表达式的'\'要写成'\\'
 (defun pomelo-better-defaults/post-init-recentf ()
   (progn
     (setq recentf-exclude
@@ -34,7 +34,8 @@
             ".*png$"
             ".*bmp$"
             ".*db$"
-            "init\\.el"))))
+            "init\\.el$"
+            "/\\.spacemacs\\.d"))))
 
 ;; 设置scratch缓冲区的默认主模式为emacs-lisp-mode
 (setq dotspacemacs-scratch-mode 'emacs-lisp-mode)
