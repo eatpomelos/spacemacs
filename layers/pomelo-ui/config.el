7d63d2b4569b19bce0305b1a0a11adfd4bc73733
pomeloly <2845748931@qq.com>
2019-12-29 22:31:07 +0800

    update

--- layers/pomelo-ui/config.el
+++ layers/pomelo-ui/config.el
@@ -1,7 +1,9 @@
-;; 设置打开emacs时的初始位置
-(set-frame-position (selected-frame) 960 0)
-(set-frame-width (selected-frame) 117)
-(set-frame-height (selected-frame) 59)
+;; 设置打开emacs时的初始位置,只有在window上运行的时候会设置这个位置，在虚拟机中不需要
+(if (spacemacs/system-is-mswindows)
+    (progn
+      (set-frame-position (selected-frame) 960 0)
+      (set-frame-width (selected-frame) 117)
+      (set-frame-height (selected-frame) 59)))
 
 ;; 设置标题
 (setq frame-title-format
